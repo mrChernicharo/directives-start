bc40bc7ca307d9d8c73e3fbf32eca3fec565d93d
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-20 17:01:38 -0300

    custom directives

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -9,16 +9,13 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    // this.renderer.setStyle(this.elRef.nativeElement,'backgroundColor', 'lightcoral')
-    // this.renderer.setStyle(this.elRef.nativeElement,'height', '40px')
-    // this.renderer.setStyle(this.elRef.nativeElement,'textAlign', 'center')
+    this.renderer.setStyle(this.elRef.nativeElement,'height', '40px')
+    this.renderer.setStyle(this.elRef.nativeElement,'textAlign', 'center')
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     this.textColor = 'white'
     this.renderer.setStyle(this.elRef.nativeElement,'backgroundColor', 'lightcoral')
-    this.renderer.setStyle(this.elRef.nativeElement,'height', '40px')
-    this.renderer.setStyle(this.elRef.nativeElement,'textAlign', 'center')
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     this.textColor = 'black'
